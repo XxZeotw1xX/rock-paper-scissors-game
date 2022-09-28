da2b6ceddeee3e3a9896ca1eaded84ce1b4cf5b5
Kristians Svikis <93152391+XxZeotw1xX@users.noreply.github.com>
2022-09-28 08:18:23 +0300

    Update game.py

--- game.py
+++ game.py
@@ -3,21 +3,25 @@ turns = ['rock', 'paper', 'scissors']
 
 
 
+while(True):
+    human_turn = input('Enter your turn, or type exit: ')
+    computer_turn = random.choice(turns)
 
-human_turn = input('Enter your turn: ')
-computer_turn = random.choice(turns)
+    if human_turn == 'exit':
+        print('Thank you for playing! Bye bye')
+        break
 
-print(f'Human:{human_turn} vs. Computer:{computer_turn}')
-if human_turn == computer_turn:
-    print('Draw!')
+    print(f'Human:{human_turn} vs. Computer:{computer_turn}')
+    if human_turn == computer_turn:
+        print('Draw!')
 
-elif human_turn == 'rock' and computer_turn == 'scissors':
-    print('Human wins!')
-elif human_turn == 'paper' and computer_turn == 'rock':
-    print('Humam wins!')
+    elif human_turn == 'rock' and computer_turn == 'scissors':
+        print('Human wins!')
+    elif human_turn == 'paper' and computer_turn == 'rock':
+        print('Humam wins!')
 
-elif human_turn == 'scissors' and computer_turn == 'paper':
-    print('Humam wins!')
+    elif human_turn == 'scissors' and computer_turn == 'paper':
+        print('Humam wins!')
 
-else:
-    print('Computer wins!')
+    else:
+        print('Computer wins!')
